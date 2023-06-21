76aa6b2a80a09ff0d35f7b1562deeadcf3cca4c2
Alban Dumouilla <alban@selecteev.io>
2023-06-21 12:20:01 +0200

    Typo fix

--- app/src/App.js
+++ app/src/App.js
@@ -38,7 +38,7 @@ function App() {
 
 
         <h3 className="text-center text-4xl mb-3 font-bold"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-9 h-9 inline-block"><path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" /></svg> Nos services</h3>
-        <h4 className="text-center text-xl mb-20 text-slate-500">Quelle que soit l'étape à laquelle vous vous trouvez, d'une simple idée à un projet bien défini,<br/>nous sommes à vos cotés pour vous aider à concrétiser vos objectifs.</h4>
+        <h4 className="text-center text-xl mb-20 text-slate-500">Quelle que soit l'étape à laquelle vous vous trouvez, d'une simple idée à un projet bien défini,<br/>nous sommes à vos côtés pour vous aider à concrétiser vos objectifs.</h4>
 
         <div className="grid grid-cols-1 lg:grid-cols-3 mb-40 gap-12 text-justify">
           <div className="card bg-base-100 shadow-xl shadow-neutral-900">
